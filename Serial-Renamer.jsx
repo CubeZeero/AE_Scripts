cc056ba971f633abc83f2888c59c1c2a42204446
Cube <48977674+CubeZeero@users.noreply.github.com>
2023-06-16 21:44:20 +0900

    Update Serial-Renamer.jsx

--- Serial-Renamer.jsx
+++ Serial-Renamer.jsx
@@ -11,6 +11,10 @@ function buildUI(thisObj){
 
     var input_text = main_wnd.add("edittext", undefined, "",{multiline : true});
     input_text.alignment = ["fill","fill"];
+    
+    var check_is = main_wnd.add("checkbox", [30, 10, 220, 30],"Ignore space (recommend)");
+    check_is.value = true
+    check_is.alignment = ["fill","bottom"];
 
     var ok_btn = main_wnd.add("button", undefined, "OK");
     ok_btn.alignment = ["fill","bottom"];
@@ -21,12 +25,12 @@ function buildUI(thisObj){
 
         var text_data = input_text.text.replace(/\r?\n/g, '')
         var activeItem = app.project.activeItem;
+        
+        if (check_is.value == true) text_data = text_data.replace(/\s+/g, "");
 
         if(activeItem){
             if(activeItem.selectedLayers.length <= text_data.length){
-                for (var i = 0; i <= Number(activeItem.selectedLayers.length); i++){
-                    activeItem.selectedLayers[i].name = text_data.charAt(i);
-                }
+                for (var i = 0; i <= Number(activeItem.selectedLayers.length); i++) activeItem.selectedLayers[i].name = text_data.charAt(i);
             }else{
                 alert("The number of characters is greater than the number of layers selected.");
             }
